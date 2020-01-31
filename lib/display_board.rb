1f384ece106440c2a03b3b536117ef0f36b8d687
Robert Aguilar <aguilar.r@live.com>
2020-01-31 05:51:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,7 +6,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |  |   "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
