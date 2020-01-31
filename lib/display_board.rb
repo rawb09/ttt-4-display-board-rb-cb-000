3a8a93c701d3da4768aa689569d1577f1dfc1f41
Robert Aguilar <aguilar.r@live.com>
2020-01-31 05:43:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
 def display_board(board)
